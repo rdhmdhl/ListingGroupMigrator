1a78746321b9dc09aafc95f7c04b0584c21886f7
Reid Hommedahl <103082829+rdhmdhl@users.noreply.github.com>
2024-04-25 14:05:14 -0700

    testing github actions

--- high_perf_pmax/lambda_main_pmax.py
+++ high_perf_pmax/lambda_main_pmax.py
@@ -1,6 +1,4 @@
 import sys
-# used for local testing
-# sys.path.append('/Users/reidhommedahl/github/lambda_layer/python/')
 import json
 from fetch_listing_groups_normal_pmax import fetch_existing_listing_groups as fetch_listing_groups_normal
 from common.initialize_google_ads_client import initialize_google_ads_client as initialize_google_ads_client_main
