2a0e1f1793c06920e7275e60cbc9624944e8df30
Reid Hommedahl <103082829+rdhmdhl@users.noreply.github.com>
2024-05-06 15:00:38 -0700

    testing

--- high_perf_pmax/lambda_main_pmax.py
+++ high_perf_pmax/lambda_main_pmax.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 # import json
 from fetch_listing_groups_normal_pmax import fetch_existing_listing_groups as fetch_listing_groups_normal
 from common.initialize_google_ads_client import initialize_google_ads_client as initialize_google_ads_client_main
