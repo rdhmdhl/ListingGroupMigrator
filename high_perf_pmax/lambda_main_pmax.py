ca4fa2a307470e4d0c8e3ef3ece1830e45d96486
Reid Hommedahl <103082829+rdhmdhl@users.noreply.github.com>
2024-05-06 14:29:50 -0700

    testing deployment again

--- high_perf_pmax/lambda_main_pmax.py
+++ high_perf_pmax/lambda_main_pmax.py
@@ -3,7 +3,7 @@ import json
 from fetch_listing_groups_normal_pmax import fetch_existing_listing_groups as fetch_listing_groups_normal
 from common.initialize_google_ads_client import initialize_google_ads_client as initialize_google_ads_client_main
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 # load env variables
 # load_dotenv()
 
